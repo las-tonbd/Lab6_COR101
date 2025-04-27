3a5b07fcfa4794cd13dcec91a6747ab122b017fd
las-tonbd <tonbd@lasbom.com>
2025-04-27 09:19:34 +0700

    Create app.tsx

---
+++ app/(tabs)/app.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function app() {
+  return (
+    <View>
+      <Text>app</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
